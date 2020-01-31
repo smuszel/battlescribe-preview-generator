9ddceed2633e1bf53382b9eb801985ecd26c0097
Stan <stan@mintlab.nl>
2020-01-31 15:26:27 +0100

    .

---
+++ index.js
@@ -0,0 +1,134 @@
+const battlescribeRoot = document.createElement('div');
+battlescribeRoot.id = 'root';
+const mainRoot = document.createElement('div');
+mainRoot.id = 'main';
+
+const psychTable = () => {
+  const temp1 = [...document.querySelectorAll('th')]
+    .filter(td => td.textContent === 'Psychic Power')
+    .map(el => el.parentElement.parentElement.parentElement)
+    .map(el => {
+      return {
+        id: name + Math.floor(Math.random() * 1000000),
+        psycherName: el.parentElement.firstElementChild.innerText.split(
+          ' ['
+        )[0],
+        cast: [...el.nextElementSibling.querySelectorAll('th')]
+          .filter(th => th.innerText === 'Cast')[0]
+          .parentElement.parentElement.querySelector('tr+tr')
+          .querySelector('td+td').innerText,
+        deny: [...el.nextElementSibling.querySelectorAll('th')]
+          .filter(th => th.innerText === 'Deny')[0]
+          .parentElement.parentElement.querySelector('tr+tr')
+          .querySelector('td+td+td').innerText,
+        powers: [...el.querySelectorAll('tbody>tr')].slice(1).map(tr => ({
+          powerName: tr.querySelector('td').innerText,
+          charge: tr.querySelector('td+td').innerText,
+          range: tr.querySelector('td+td+td').innerText,
+        })),
+      };
+    });
+
+  const psycherPowers = temp1
+    .flatMap(psy => {
+      return psy.powers.map(power => ({
+        ...psy,
+        power,
+        powers: null,
+      }));
+    })
+    .sort((itemA, itemB) => {
+      if (itemA.power.powerName === 'Smite' && itemA.id === itemB.id) {
+        return 1;
+      } else if (itemB.power.powerName === 'Smite' && itemB.id === itemA.id) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+
+  const table = document.createElement('table');
+  const idCache = new Set();
+
+  psycherPowers.forEach(
+    ({ id, cast, deny, psycherName, power: { powerName, charge, range } }) => {
+      const row = document.createElement('tr');
+      const cells = Array(6)
+        .fill()
+        .map(() => document.createElement('td'));
+
+      cells[0].innerText = psycherName + ` (${cast}/${deny})`;
+      cells[0].className = 'psycherCell';
+      cells[1].className = 'powerCell';
+      cells[1].innerText =
+        // range === '18"' && charge === '5'
+        powerName === 'Smite'
+          ? powerName
+          : `${powerName} (${charge}/${range === 'N/A' ? '*' : range})`;
+      cells.slice(2).forEach(cell => {
+        cell.className = 'testCell';
+      });
+
+      if (idCache.has(id)) {
+        cells.splice(0, 1);
+      } else {
+        cells[0].rowSpan = psycherPowers.filter(psy2 => id === psy2.id).length;
+        idCache.add(id);
+      }
+
+      cells.forEach(cell => row.appendChild(cell));
+      table.appendChild(row);
+    }
+  );
+
+  setTimeout(() => {
+    battlescribeRoot.style.display = 'none';
+  }, 10);
+
+  return table;
+};
+const fileUpload = () => {
+  const upload = document.createElement('input');
+  upload.type = 'file';
+
+  return {
+    promise: new Promise(rez => {
+      upload.addEventListener('change', () => {
+        const file = upload.files[0];
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.addEventListener('load', () => {
+          battlescribeRoot.innerHTML = reader.result;
+          setTimeout(rez, 0);
+        });
+      });
+    }),
+    upload,
+  };
+};
+const cpMarkers = () => {
+  const cpsContainer = document.createElement('div');
+  cpsContainer.className = 'cpsContainer';
+  const markers = Array(9)
+    .fill()
+    .map(() => {
+      const cpMarker = document.createElement('div');
+      cpMarker.className = 'cpmarker';
+      return cpMarker;
+    });
+  markers.forEach(marker => cpsContainer.appendChild(marker));
+  return cpsContainer;
+};
+
+const { promise, upload } = fileUpload();
+
+battlescribeRoot.appendChild(upload);
+document.body.appendChild(mainRoot);
+document.body.appendChild(battlescribeRoot);
+
+promise.then(() => {
+  const table = psychTable();
+  const markers = cpMarkers();
+  mainRoot.appendChild(table);
+  mainRoot.appendChild(markers);
+});
